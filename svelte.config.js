8bdd2506821acd04f075b503069a9fab4ace897c
r_g7d <danangr123@gmail.com>
2024-11-15 19:51:24 +0700

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+      		assets: 'build',
+			fallback: null
+		}),
 		paths: {
 		  base: process.env.NODE_ENV === "production" ? "/color-outfit-app" : "",
 		}
